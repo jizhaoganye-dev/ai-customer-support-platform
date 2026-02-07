737bdd588eea14b4c76eab2d02351b7edf7b9ca8
jizhaoganye-dev <jizhaoganye@gmail.com>
2026-02-08 08:50:11 +0900

    Simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
   images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
-    formats: ['image/avif', 'image/webp'],
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    unoptimized: true,
   },
 }
 
