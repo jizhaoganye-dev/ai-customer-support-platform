7111facf026b091437b7e431349c6e1cd16b9ca8
jizhaoganye-dev <jizhaoganye@gmail.com>
2026-02-08 17:11:02 +0900

    update jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+}
+
+export default createJestConfig(config)
